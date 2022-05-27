30895494cc5f0638b7fe7a023b96f444ecf5e530
jaime zuber <jaimezuber99@gmail.com>
2022-05-26 21:15:55 -0300

    seeds hechas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = %w[chinese italian japanese french belgian]
+
+Restaurant.destroy_all
+10.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: categories.sample)
+end
