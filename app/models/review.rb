cc00bb24c3032730ae6df2ab65ca153a87b04d11
jaime zuber <jaimezuber99@gmail.com>
2022-05-26 21:06:22 -0300

    me falto guardar el utlimo cambio

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true
   validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: 0..5
 end
