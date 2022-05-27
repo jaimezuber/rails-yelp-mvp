8f8804ac00d8042e2366f30ea603de6d9926bd6b
jaime zuber <jaimezuber99@gmail.com>
2022-05-26 23:19:21 -0300

    Le agregue collection al form_for para probar

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  CATEGORIES = %w[chinese italian japanese french belgian]
+
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
